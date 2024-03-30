f691180d448cd37495cdffae90f271e7debed7b7
shobhit <shobhitsharma.3661@gmail.com>
2024-03-30 21:56:16 +0530

    first changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import ContactDetails from "./Components/ContactDetails";
+import Experience from "./Components/Experience";
+import Home from "./Components/Home/Home";
+import Projects from "./Components/Projects";
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+      <Experience></Experience>
+      <Projects></Projects>
+      <ContactDetails></ContactDetails>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className="homeClass">
+        <div className="background">
+          <img
+            className="backgroundImage"
+            src="topBackground.jpg"
+            alt="logo192.png"
+          />
+          <div className="nameDetails">
+            <h1>I'm Shobhit Sharma</h1>
+            <h3>A Software Engineer</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
